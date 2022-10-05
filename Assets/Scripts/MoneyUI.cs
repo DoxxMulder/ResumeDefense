929350ae124cf147ffb682fb1fe7e171e0088e67
DoxxMulder <16737434+DoxxMulder@users.noreply.github.com>
2022-10-04 17:54:37 -0700

    Added money display

--- Assets/Scripts/MoneyUI.cs
+++ Assets/Scripts/MoneyUI.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoneyUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TextMeshProUGUI moneyText;
 
     // Update is called once per frame
     void Update()
     {
-        
+        moneyText.text = "$" + PlayerStats.Money.ToString();
     }
 }
