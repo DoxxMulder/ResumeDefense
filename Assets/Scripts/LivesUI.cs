b869905420678cc9a145f0ba1ba463fd92b4160f
DoxxMulder <16737434+DoxxMulder@users.noreply.github.com>
2022-10-06 10:17:57 -0700

    Added UI element to display 'Lives'

--- Assets/Scripts/LivesUI.cs
+++ Assets/Scripts/LivesUI.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LivesUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TextMeshProUGUI livesText;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        livesText.text = "Lives: " + PlayerStats.Lives;
     }
 }
