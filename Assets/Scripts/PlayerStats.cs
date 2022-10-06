ea1eed5d895716667f9ead16ffa389f1e95c918b
DoxxMulder <16737434+DoxxMulder@users.noreply.github.com>
2022-10-06 10:15:27 -0700

    Added 'Lives'

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -7,8 +7,12 @@ public class PlayerStats : MonoBehaviour
     public static int Money;
     public int startMoney = 400;
 
+    public static int Lives;
+    public int startLives = 10;
+
     void Start()
     {
         Money = startMoney;
+        Lives = startLives;
     }
 }
