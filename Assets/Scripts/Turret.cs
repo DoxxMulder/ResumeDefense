57ecc6391a16d63c4bffb7eb689560a9efc75a63
DoxxMulder <16737434+DoxxMulder@users.noreply.github.com>
2022-10-08 10:48:19 -0700

    Added laser type to turret script

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -5,20 +5,26 @@ using UnityEngine;
 public class Turret : MonoBehaviour
 {
     
-    [Header("Attributes")]
+    [Header("General")]
 
     public float range = 15f;
+
+    [Header("Use Bullets (default)")]
+    public GameObject bulletPrefab;
     public float fireRate = 1f;
     private float fireCountdown = 0f;
+
+    [Header("Use Laser")]
+    public bool useLaser = false;
+    public LineRenderer lineRenderer;
 
     [Header("Unity Setup Fields")]
 
     public Transform target;
     public Transform partToRotate;
     public float turnSpeed = 10f;
-    private string enemyTag = "Enemy";
+    public string enemyTag = "Enemy";
 
-    public GameObject bulletPrefab;
     public Transform firePoint;
 
     // Start is called before the first frame update
