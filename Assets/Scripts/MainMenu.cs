0a2742d41e1e8faa9d233f0163aecd081c335095
DoxxMulder <16737434+DoxxMulder@users.noreply.github.com>
2022-10-18 10:54:35 -0700

    Added main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        Debug.Log("Play");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+    }
+}
