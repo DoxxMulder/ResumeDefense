ddb31060940b1de5ec04dd68ca34c155423ced16
DoxxMulder <16737434+DoxxMulder@users.noreply.github.com>
2022-10-20 09:57:16 -0700

    Added scene fade-in

---
+++ Assets/Scripts/SceneFader.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneFader : MonoBehaviour
+{
+    public Image image;
+
+    void Start()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    IEnumerator FadeIn()
+    {
+        float t = 1f;
+
+        while (t > 0f)
+        {
+            t -= Time.deltaTime;
+            image.color = new Color(0f, 0f, 0f, t);
+            yield return 0;
+        }
+    }
+}
