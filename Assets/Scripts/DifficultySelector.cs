13ce6618218e2da354097abba0f6ebf50791fad2
DoxxMulder <16737434+DoxxMulder@users.noreply.github.com>
2022-11-07 12:21:33 -0800

    Added DifficultySelector functionality

--- Assets/Scripts/DifficultySelector.cs
+++ Assets/Scripts/DifficultySelector.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DifficultySelector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public SceneFader fader;
+
+    public Button leftButton;
+    public Button rightButton;
+
+    public TextMeshProUGUI difficulty;
+    public TextMeshProUGUI difficultySubtitle;
+    public TextMeshProUGUI difficultyDescription;
+
+    public string[] difficultyList = new string[3]{ "Easy", "Medium", "Hard" };
+    private int difficultyIndex = 0;
+    
+    public void DifficultyLeft()
     {
-        
+        if (difficultyIndex == 0)
+        {
+            leftButton.interactable = false;
+            return;
+        }
+        else
+        {
+            rightButton.interactable = true;
+            difficultyIndex--;
+            difficulty.text = difficultyList[difficultyIndex];
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DifficultyRight()
     {
-        
+        if (difficultyIndex == difficultyList.Length-1)
+        {
+            rightButton.interactable = false;
+            return;
+        }
+        else
+        {
+            leftButton.interactable = true;
+            difficultyIndex++;
+            difficulty.text = difficultyList[difficultyIndex];
+        }
+    }
+
+    public void Continue()
+    {
+        PlayerPrefs.SetInt("Difficulty", difficultyIndex);
+        fader.FadeTo("LevelSelect");
     }
 }
